beb58deed631ca00a2f0085ebf4b2adaeefb061a
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-04-12 23:38:34 -0400

    added feedbacks

--- src/pages/landing-page/ClientCarousel.tsx
+++ src/pages/landing-page/ClientCarousel.tsx
@@ -3,7 +3,7 @@ function ClientCarousel() {
   return (
     // <div className=" flex justify-center border-green-500 border w-full">
     <div className="w-full p-4 flex justify-center items-center border-purple-400 border">
-      <img src={client1} className="grayscale-[50]" alt="" />
+      <img src={client1} className="grayscale-[50] filter" alt="" />
     </div>
     // </div>
   );
