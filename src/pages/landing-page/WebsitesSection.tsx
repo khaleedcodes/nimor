f78c86b594bf9b555a4b34fc5a027b4ad96e1d1b
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-04-15 15:51:03 -0400

    dded form

--- src/pages/landing-page/WebsitesSection.tsx
+++ src/pages/landing-page/WebsitesSection.tsx
@@ -5,7 +5,7 @@ import Button from "../../components/Button";
 function WebsitesSection() {
   return (
     <div className="flex justify-center">
-      <div className="max-w-screen-xl w-full p-4 flex flex-col justify-center items-center gap-20">
+      <div className="max-w-screen-xl w-full p-4 flex flex-col justify-center items-center gap-10">
         <SectionHeader>You name it, we build it.</SectionHeader>
         <WebsiteList />
         <Button classes="pt-3 pb-3 pl-10 pr-10">
