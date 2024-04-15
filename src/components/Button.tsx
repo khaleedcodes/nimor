c4234c42241976bef823a9daa15a4096e1ee6c7b
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-04-15 06:56:30 -0400

    dded form

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import { ButtonProp } from "../types/types";
 
 function Button({ children, classes }: ButtonProp) {
   return (
-    <button
-      className={`border transition-colors duration-300 bg-first-accent text-white rounded-md hover:bg-second-accent ${classes}`}
-    >
-      {children}
-    </button>
+    <Link to={`https://forms.gle/2YPmyTGXvdDSJHU19`}>
+      <button
+        className={`border transition-colors duration-300 bg-first-accent text-white rounded-md hover:bg-second-accent ${classes}`}
+      >
+        {children}
+      </button>
+    </Link>
   );
 }
 
