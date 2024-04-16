8597d5916ecfe796963c6082aec3ebc2020c8a44
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-04-15 22:33:48 -0400

    added google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,15 @@ import App from "./App.tsx";
 import "./styles/index.css";
 import "./styles/gradient.css";
 
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-J6LSCJYGTZ");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+  title: "pageview from main.tsx",
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
