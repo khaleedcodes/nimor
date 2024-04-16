71ac664434e784e3a623ff2170fdd0e3436f2fec
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-04-15 21:57:26 -0400

    added error page

--- src/pages/error-page/ErrorPage.tsx
+++ src/pages/error-page/ErrorPage.tsx
@@ -1,7 +1,22 @@
+import BackIcon from "../../assets/icons/BackIcon";
 function ErrorPage() {
   return (
-    <div>ErrorPage</div>
-  )
+    <div className="min-h-lvh flex items-center justify-center bg-black gap-4">
+      <div>
+        <p className="text-white text-5xl font-bold border-r pr-4">404</p>
+      </div>
+      <div className="flex flex-col justify-start">
+        <p className=" text-white  font-bold">Resource not found</p>
+        <a
+          href="/"
+          className=" flex  items-center justify-center gap-2 max-w-max text-first-accent duration-300 transition-all border-b border-b-transparent hover:border-b-first-accent"
+        >
+          <BackIcon />
+          Back to our site
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
