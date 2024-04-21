c10159e7fde4c371a4a0737dd19aa73a6109b31f
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-04-21 17:15:29 -0400

    added shadow

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import WordMark from "./WordMark";
 import Button from "./Button";
 function NavBar() {
   return (
-    <div className="sticky top-0 flex justify-center bg-white z-10">
+    <div className="sticky top-0 flex justify-center bg-white z-20">
       <nav className="justify-between items-center flex p-4 w-full">
         <WordMark classes="text-black" />
         <div className="flex items-center gap-8">
