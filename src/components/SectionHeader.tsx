497e3f10f61d560b297dbef2defc3454fd7044e5
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-04-22 18:06:48 -0400

    changed header section size on smaller screen

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -2,7 +2,7 @@ import { SectionHeaderProp } from "../types/types";
 function SectionHeader({ children }: SectionHeaderProp) {
   return (
     <div className="">
-      <h1 className="title-gradient text-5xl font-bold text-center">
+      <h1 className="title-gradient text-5xl max-sm:text-3xl font-bold text-center">
         {children}
       </h1>
     </div>
